3ae7f1e76183e032ee7b645321b8401f6c48f457
unknown <mtusxl@gmail.com>
2024-03-07 18:28:01 +0300

    Fixed styles

--- env/Scripts/app/models.py
+++ env/Scripts/app/models.py
@@ -1,11 +1,11 @@
-
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    
+     
+  
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, nullable=False)
     login = db.Column(db.String(80), unique=True, nullable=False)
--- env/Scripts/main.py
+++ env/Scripts/main.py
@@ -1,23 +1,21 @@
 from flask import Flask
+from flask_migrate import Migrate
 from app.regist_views import regist
 from app.signin import auth
 from app.today_views import today_bp
 from app.models import db
 
-
 def create_app():
     app = Flask(__name__, template_folder='app/templates')
-    
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Users.db'
     db.init_app(app)
-    
+    migrate = Migrate(app, db)  # Добавляем инициализацию Flask-Migrate
 
     app.register_blueprint(regist)
     app.register_blueprint(auth)
     app.register_blueprint(today_bp)
 
     return app
-
 
 if __name__ == '__main__':
     app = create_app()
